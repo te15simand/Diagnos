f0ffa9ae48aa28e68d93ccf78a3e39d046f841d6
Simon Andersson <te15simand@falufri.se>
2017-03-09 14:40:57 +0100

    Add contact page

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ app = Flask("webapp")
 
 @app.route("/")
 def home():
-	return render_template("base.html")
-
-
-@app.route("/index/")
-def index():
 	return render_template("index.html")
+
+
+@app.route("/contact/")
+def index():
+	return render_template("contact.html")
 
 if __name__ == "__main__":
 	app.run()
